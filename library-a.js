7f545bad1c4d89e5e5d04b71aa2d878b072a9778
Zaki Wasik <zaki@webconsu.lt>
2017-07-23 10:22:22 +0200

    first commit

---
+++ library-a.js
@@ -0,0 +1,15 @@
+function invoke() {
+  return "invoke";
+}
+
+function call() {
+  return "call";
+}
+
+function clear() {
+  return "clear";
+}
+
+function render() {
+  return "render a";
+}
---
+++ library-b.js
@@ -0,0 +1,15 @@
+function initialize() {
+  return "initialize";
+}
+
+function fill() {
+  return "fill";
+}
+
+function rebuild() {
+  return "rebuild";
+}
+
+function render() {
+  return "render b";
+}
